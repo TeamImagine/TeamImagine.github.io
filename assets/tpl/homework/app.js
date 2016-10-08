8b1be60736b47acc6f40c9fd76541ff3b3f47424
MurphyL <im@murphyl.com>
2016-10-08 18:41:36 +0800

    get template by ajax

--- assets/tpl/homework/app.js
+++ assets/tpl/homework/app.js
@@ -43,36 +43,7 @@ var initDatagrid = function() {
         onLoadSuccess: function(){
             $('.do-answer').click(function(){
                 // window.open('homework/answer?id=' + this.getAttribute('data-id'));
-                $.post('https://api.github.com/repos/TeamImagine/TeamImagine.github.io/issues', {
-                  "title": "Found a bug",
-                  "body": "I'm having a problem with this.",
-                  "assignee": "octocat",
-                  "assignees": [
-                    {
-                      "login": "octocat",
-                      "id": 1,
-                      "avatar_url": "https://github.com/images/error/octocat_happy.gif",
-                      "gravatar_id": "",
-                      "url": "https://api.github.com/users/octocat",
-                      "html_url": "https://github.com/octocat",
-                      "followers_url": "https://api.github.com/users/octocat/followers",
-                      "following_url": "https://api.github.com/users/octocat/following{/other_user}",
-                      "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
-                      "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
-                      "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
-                      "organizations_url": "https://api.github.com/users/octocat/orgs",
-                      "repos_url": "https://api.github.com/users/octocat/repos",
-                      "events_url": "https://api.github.com/users/octocat/events{/privacy}",
-                      "received_events_url": "https://api.github.com/users/octocat/received_events",
-                      "type": "User",
-                      "site_admin": false
-                    }
-                  ],
-                  "milestone": 1,
-                  "labels": [
-                    "bug"
-                  ]
-                }, (data) => {
+                $.post('https://api.github.com/authorizations', (data) => {
                     console.log(data)
                 });
             });
