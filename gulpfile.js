7daec8f2634ccd46f4a0975fe1e6416e1253651c
MurphyL <im@murphyl.com>
2016-10-08 14:45:55 +0800

    moment.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,21 @@ var gulp = require('gulp'),
 
 gulp.task('clean', function () {
 	return gulp.src([
-  		'assets/lib/*.css'
+  		'assets/lib/*.css',
+  		'assets/lib/moment'
   	], {read: false}).pipe(clean());
 });
 
 gulp.task('dest', ['clean'], function(){
 	gulp.src([
 		'node_modules/*.css/*.css',
-		'node_modules/*.css/LICENSE*'
+		'node_modules/*.css/LICENSE*',
+		'node_modules/moment/min/moment-with-locales.min.js'
 	]).pipe(gulp.dest('assets/lib'));
+	gulp.src([
+		'node_modules/moment/LICENSE',
+		'node_modules/moment/min/moment-with-locales.min.js'
+	]).pipe(gulp.dest('assets/lib/moment'));
 });
 
 gulp.task('default', ['dest']);
