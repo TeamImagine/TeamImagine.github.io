8d89c13e0f301230d9e65a19036588c7b8f4a234
MurphyL <im@murphyl.com>
2016-10-08 16:56:44 +0800

    get template by ajax

--- assets/js/system.js
+++ assets/js/system.js
@@ -1,71 +1,130 @@
-var setBakground = function (src){
-    if ($.trim(src).length > 0) {
-        $('#homepage').css({
-            'background': 'url(' + src + ')',
-            'background-attachment': 'fixed',
-            'background-position': 'center center',
-            'background-size': 'cover'
-        });
-    }
-};
+/**
+ * 生成桌面图标的窗口的载体，练习使用前端模板引擎，实际上使用 PHP 会方便
+ **/
+$(() => {
 
-$(function() {
-    /***
-     * 设置背景
-     */
-    (setBakground)(localStorage.getItem('background-img-src'));
-    /***
-     * 屏幕解锁 - start
-     */
+    (function (src){
+        if ($.trim(src).length > 0) {
+            $('#homepage').css({
+                'background': 'url(' + src + ')',
+                'background-attachment': 'fixed',
+                'background-position': 'center center',
+                'background-size': 'cover'
+            });
+        }
+    })(localStorage.getItem('background-img-src'));
+
     setTimeout(function() {
         $('#mask').fadeOut(50);
-    }, 1000);
-    /***
-     * 屏幕解锁 - end
-     */
-    /***
-     * 桌面按钮 - start
-     */
-    // 计算桌面的矩阵的坐标，偏移量
-    window.matrix_offset_x = 10;
-    window.matrix_offset_y = 10;
-    $('#matrix .item').each(function(i, o) {
-        if (i > 0 && i % 9 === 0) { // 换列，重新计算偏移量
-            window.matrix_offset_x += 80;
-            window.matrix_offset_y = 10;
-        }
-        $(o).css({
-            top: window.matrix_offset_y + 'px',
-            left: window.matrix_offset_x + 'px'
-        }).click(function(e) { // 绑定桌面图标点击事件
-            var _this = $(e.target).closest('.item'),
-                wid = o.id + '-window',
-                tid = o.id + '-task-list-item',
-                url = '/asserts/tpl/' + o.id + '/';
-            $('#' + wid).window('open', 'center'); // 弹出层居中
-            // 增加任务栏图标
-            if ($('#' + tid).length === 0) {
-                var iconHtml = '<a id="' + tid + '" class="task-item" href="javascript:;"></a>';
-                $('#task-list').append($(iconHtml));
-                $('#' + tid).click(function() {
-                    if (!navigator.onLine) {
-                        alert('网络已离线...');
-                    }
-                    var _win = $('#' + wid);
-                    // 点击状态栏按钮触发不同的动作
-                    if (_win.hasClass('maximized')) {
-                        _win.window('open');
-                    } else {
-                        _win.window('minimize');
-                    }
-                });
+    }, 2500);
+
+    // 弹窗相关
+    var windowWidth = $(document).width(),
+        windowHeight = $(document).height(),
+        matrixOffsetX = 10, matrixOffsetY = 10;
+    if (windowHeight > windowWidth) { // 避免出现显示器的宽度小于高度的情况，好在我是双屏，遇到了这个问题
+        windowHeight = windowWidth * 0.8;
+    }
+    windowHeight = windowHeight * 0.7;
+
+    var renderWindow = (conf) => {
+        var ele = $('<li>');
+        $('#window-queen').append(ele);
+        ele.window({
+            id: conf.id + '-window',
+            title: conf.name,
+            width: windowHeight / 0.618,
+            height: windowHeight,
+            closed: true,
+            cache: true,
+            href: '/assets/tpl/' + conf.id + '/index.html',
+            modal: false,
+            collapsible: false,
+            onOpen: function(){
+                ele.removeClass('maximized');
+            },
+            onMinimize: function(){
+                $('#' + this.id).addClass('maximized');
+            },
+            onBeforeClose: function(){
+                $('#' + conf.id + '-task-list-item').remove();
             }
         });
-        window.matrix_offset_y += 80; // 累加纵向的偏移量
+        
+    };
+
+    $.get('/icons.json', (icons) => {
+        icons.forEach((icon, index) => {
+            renderWindow(icon, i);
+            $('#' + icon.id).click(() => {
+                $('#' + icon.id + '-window').window('open')
+            });
+            // 桌面图标换列，重新计算偏移量
+            if (i > 0 && i % 9 === 0) { 
+                matrixOffsetX += 80;
+                matrixOffsetY = 10;
+            }
+            $('#' + icon.id).css({
+                top: matrixOffsetX + 'px',
+                left: matrixOffsetY + 'px'
+            }).click(function(e) { // 绑定桌面图标点击事件
+                var _this = $(e.target).closest('.item'),
+                    wid = icon.id + '-window',
+                    win = $('#' + wid),
+                    tid = icon.id + '-task-list-item',
+                    url = '/asserts/tpl/' + icon.id + '/';
+                $('#' + wid).window('open', 'center'); // 弹出层居中
+                // 增加任务栏图标
+                if ($('#' + tid).length === 0) {
+                    var iconHtml = '<a id="' + tid + '" class="task-item" href="javascript:;"></a>';
+                    $('#task-list').append($(iconHtml));
+                    $('#' + tid).click(function() {
+                        if (!navigator.onLine) {
+                            alert('网络已离线...');
+                        }
+                        // 点击状态栏按钮触发不同的动作
+                        if (wid.hasClass('maximized')) {
+                            wid.window('open');
+                        } else {
+                            wid.window('minimize');
+                        }
+                    });
+                }
+            });
+            matrixOffsetX += 80; // 累加纵向的偏移量
+        });
     });
+
+
     /***
-     * 桌面按钮 - end
+     * 桌面图标特效
      */
+    $('.item').hover(function(){
+        var _this = $(this),
+            clazz = 'item-window-animated pulse';
+        _this.addClass(clazz);
+        setTimeout(function(){
+            _this.removeClass(clazz);
+        }, 1000);
+    });
+
+    /**
+     * 桌面右键菜单
+     */
+    $('body').bind('contextmenu',function(e){
+        e.preventDefault();
+        if('homepage' === e.target.id){
+            $('#mm').menu('show', {
+                left: e.pageX,
+                top: e.pageY
+            });
+        }
+        return false;
+    });
+
+    $('#background-menu-item').click(function(){
+        $('#background-window-container').window('open');
+    });
     setInterval(function() {
         $('#system-clock').text(moment().format('HH:mm'));
         if (!navigator.onLine) {
@@ -73,3 +132,23 @@ $(function() {
         }
     }, 1000);
 });
+
+var messager = {
+    slide: function(title, text) {
+        return $.messager.show({
+            title: title,
+            msg: text,
+            showType: 'slide',
+            style: {
+                bottom: 40,
+                top: '',
+                left: '',
+                right: 5
+            }
+        });
+    },
+    confirm: function(title, message, cb) {
+        return $.messager.confirm(title, message, cb);
+    }
+};
+
