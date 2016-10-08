fa5502a9a0f52d886d392d4e8fd3d5562675d6f6
MurphyL <im@murphyl.com>
2016-10-08 19:23:47 +0800

    get template by ajax

--- assets/tpl/homework/app.js
+++ assets/tpl/homework/app.js
@@ -43,7 +43,7 @@ var initDatagrid = function() {
         onLoadSuccess: function(){
             $('.do-answer').click(function(){
                 // window.open('homework/answer?id=' + this.getAttribute('data-id'));
-                $.post('https://api.staticman.net/v1/entry/TeamImagine/TeamImagine.github.io/test', $({fields: {title: 'a', description: 'description'}}).serialize(), (data) => {
+                $.post('https://api.staticman.net/v1/entry/TeamImagine/TeamImagine.github.io/test', {fields: {title: 'a', description: 'description'}}, (data) => {
                     console.log(data)
                 });
             });
