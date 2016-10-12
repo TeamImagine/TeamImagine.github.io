d564447033297897daf33ef2bd4a36ce83a2c186
MurphyL <im@murphyl.com>
2016-10-12 13:22:39 +0800

    重新组织文件

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ gulp.task('clean', function () {
 	return gulp.src([
   		'assets/lib/*.css',
   		'assets/lib/moment',
+  		'assets/lib/simplemde',
   		'assets/lib/nunjucks'
   	], {read: false}).pipe(clean());
 });
@@ -14,15 +15,12 @@ gulp.task('dest', ['clean'], function(){
 		'node_modules/*.css/*.css',
 		'node_modules/*/LICENSE*',
 	]).pipe(gulp.dest('assets/lib'));
-	gulp.src([
-		'node_modules/medium-editor/dist/*/medium-editor.min.*'
-	]).pipe(gulp.dest('assets/lib/medium-editor'));
-	gulp.src([
-		'node_modules/moment/min/moment-with-locales.min.js'
-	]).pipe(gulp.dest('assets/lib/moment'));
-	gulp.src([
-		'node_modules/nunjucks/browser/nunjucks.min.js'
-	]).pipe(gulp.dest('assets/lib/nunjucks'));
+	gulp.src('node_modules/simplemde/dist/*')
+		.pipe(gulp.dest('assets/lib/simplemde'));
+	/*gulp.src('node_modules/nunjucks/browser/nunjucks.min.js')
+		.pipe(gulp.dest('assets/lib/nunjucks'));*/
+	gulp.src('node_modules/moment/min/moment-with-locales.min.js')
+		.pipe(gulp.dest('assets/lib/moment'));
 });
 
 gulp.task('default', ['dest']);
