b1956ba769e6347ed5122a6b7aed2f50628db5bd
MurphyL <im@murphyl.com>
2016-10-12 14:05:36 +0800

    编辑器

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,8 @@ gulp.task('clean', function () {
 	return gulp.src([
   		'assets/lib/*.css',
   		'assets/lib/moment',
-  		'assets/lib/simplemde',
-  		'assets/lib/nunjucks'
+  		'assets/lib/nunjucks',
+  		'assets/lib/codemirror'
   	], {read: false}).pipe(clean());
 });
 
@@ -15,8 +15,11 @@ gulp.task('dest', ['clean'], function(){
 		'node_modules/*.css/*.css',
 		'node_modules/*/LICENSE*',
 	]).pipe(gulp.dest('assets/lib'));
-	gulp.src('node_modules/simplemde/dist/*')
-		.pipe(gulp.dest('assets/lib/simplemde'));
+	gulp.src([
+		'node_modules/codemirror/lib/*',
+		'node_modules/codemirror/mode/markdown/*',
+	])
+		.pipe(gulp.dest('assets/lib/codemirror'));
 	/*gulp.src('node_modules/nunjucks/browser/nunjucks.min.js')
 		.pipe(gulp.dest('assets/lib/nunjucks'));*/
 	gulp.src('node_modules/moment/min/moment-with-locales.min.js')
